bf86476af3b3f197e4214323efcb749572bb53c2
PREMANSHU05 <premanshupadwal@gmail.com>
2026-07-19 16:51:55 +0530

    upadated

--- init/index.js
+++ init/index.js
@@ -18,7 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({...obj,owner: '69cbdc67cacc21d5268c2735'}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69cbdc67cacc21d5268c2735",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
